3d2fc2736f2c4311820826e40e0fd007f8f34e6d
João Scussiato <joao@scussiatoe.com>
2023-11-30 14:59:18 -0300

    Tela individual de produto funcionando

--- setup/settings.py
+++ setup/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "produtos",
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,7 @@ ROOT_URLCONF = "setup.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -110,7 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static') 
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'static/styles') 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
